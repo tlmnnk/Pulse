3ee1b20f05295de849383c9d41486ec0818600e2
vladislav t <vt118@mail.ru>
2019-07-19 00:23:50 +0300

    start to make catalog section

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,23 @@
-$(document).ready(function() {
-    $('.carousel__inner').slick({
-        speed: 600,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/left-arrow.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/right-arrow.png"></button>',
-        responsive: [{
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-    });
+ $(document).ready(function() {
+     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+       $(this)
+         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+         });
+ });
+
+var slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: false
+
+});
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
 });
